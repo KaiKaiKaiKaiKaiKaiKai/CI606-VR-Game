8fff2e8f7f1429738bbcaafd7736ec8c6aacbfdf
Kai Nicholas <mail@kainicholas.co.uk>
2024-01-12 13:41:40 +0000

    made flood water trigger on exit, not enter

--- VR Game/Assets/Scripts/Objects/FloodWater.cs
+++ VR Game/Assets/Scripts/Objects/FloodWater.cs
@@ -8,7 +8,7 @@ public class FloodWater : MonoBehaviour
     // Define an event of type CollisionHandler
     public event CollisionHandler OnCollisionOccurred;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerExit(Collider other)
     {
         // When the trigger is entered, raise the event and pass the collided GameObject
         OnCollisionOccurred?.Invoke(other.gameObject);
